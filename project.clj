dd581ea797c8555e073fd5ada0b2a5cefd7e0532
Kenneth Tilton <kentilton@gmail.com>
2023-03-29 11:26:27 -0400

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,19 +3,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/kennytilton/web-mx/"
-  :dependencies [[org.clojure/clojure "1.11.0"]
-                 [org.clojure/clojurescript "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.60"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/core.async "1.6.673"]
                  [org.slf4j/slf4j-nop       "1.7.30"]
-                 [com.cognitect/transit-cljs "0.8.280"]
+                 [com.cognitect/transit-cljs "0.8.264"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
                  [funcool/bide "1.7.0"]
                  [clj-http "3.10.3"]
                  [cljs-http "0.1.46"]
-                 [cheshire "5.10.0"]
+                 [cheshire "5.11.0"]
                  [com.taoensso/tufte "2.4.5"]
-                 [com.tiltontec/matrix "5.0.0-SNAPSHOT"]
-                 [com.tiltontec/web-mx "2.0.0-SNAPSHOT"]
-                 [com.tiltontec/mxxhr "2.0.0-SNAPSHOT"]]
+                 [com.tiltontec/matrix "5.0.1-SNAPSHOT"]
+                 [com.tiltontec/web-mx "2.0.1-SNAPSHOT"]]
   :profiles
   {:dev
    {:dependencies [[org.clojure/clojurescript "1.10.773"]
